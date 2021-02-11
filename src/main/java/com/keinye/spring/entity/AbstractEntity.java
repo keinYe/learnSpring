8fa465c55236cc810933bb6453bb5afb3fc6f8c8
keinYe <keinye.dev@gmail.com>
2021-02-11 22:01:14 +0800

    update AbstractEntity

--- src/main/java/com/keinye/spring/entity/AbstractEntity.java
+++ src/main/java/com/keinye/spring/entity/AbstractEntity.java
@@ -1,5 +1,50 @@
 package com.keinye.spring.entity;
 
-public class AbstractEntity {
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.Transient;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
+	private Integer id;
+	
+	@Column(nullable = false, updatable = false)
+	private Long createdAt;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public Long getCreatedAt() {
+		return createdAt;
+	}
+	
+	@Transient
+    public ZonedDateTime getCreatedDateTime() {
+        return Instant.ofEpochMilli(this.createdAt).atZone(ZoneId.systemDefault());
+    }	
+	
+	private void setCreatedAt(Long createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	@PrePersist
+    public void preInsert() {
+        setCreatedAt(System.currentTimeMillis());
+    }	
 
 }
