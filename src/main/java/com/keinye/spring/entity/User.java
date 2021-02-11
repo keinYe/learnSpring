9b397c1a0e4cf89d9ab80af42febcf740ac0d93f
keinYe <keinye.dev@gmail.com>
2021-02-11 22:01:14 +0800

    修改 springboot 版本，增加 readme 文件

--- src/main/java/com/keinye/spring/entity/User.java
+++ src/main/java/com/keinye/spring/entity/User.java
@@ -1,43 +1,21 @@
 package com.keinye.spring.entity;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
-import org.hibernate.event.spi.PreInsertEvent;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User extends AbstractEntity{
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(nullable = false, updatable = false)
-	private Integer id;
 	
 	@Column(nullable = false, unique = false, length = 100)
 	private String name;
 	@Column(nullable = false, length = 100)
 	private String password;
-	@Column(nullable = false, updatable = false)
-	private Long createdAt;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
@@ -55,24 +33,6 @@ public class User {
 		this.password = password;
 	}
 
-	public Long getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Long createdAt) {
-		this.createdAt = createdAt;
-	}
-	
-	@Transient
-	public String getCreatedDateTime() {
-		return Instant.ofEpochMilli(this.createdAt).atZone(ZoneId.systemDefault())
-				.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-	}
-	
-	@PrePersist
-	public void preInsert() {
-		setCreatedAt(System.currentTimeMillis());
-	}
 	
 	@Override
 	public String toString() {
