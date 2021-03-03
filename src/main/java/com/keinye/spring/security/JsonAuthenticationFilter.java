a9d314e2a9e2619480249a1ec0318d370d03bf41
keinYe <keinye.dev@gmail.com>
2021-03-03 22:05:23 +0800

    add file

---
+++ src/main/java/com/keinye/spring/security/JsonAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.keinye.spring.security;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JsonAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+	private boolean postOnly = true;
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		if (this.postOnly && !request.getMethod().equals("POST")) {
+			throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+		}
+		
+		return super.attemptAuthentication(request, response);
+	}
+}
